e2b1dec69748b7317cb6a21478057a3ffe3f1b15
Sunshine-Prachi <prachirathod1764@gmail.com>
2024-06-12 22:40:16 +0530

    QA lead pushing the code to master branch

---
+++ src/test/java/testPackage/Login_Class.java
@@ -0,0 +1,10 @@
+package testPackage;
+
+public class Login_Class {
+
+	public static void main(String[] args) {
+		System.out.println("This is my Login class");
+
+	}
+
+}
---
+++ src/test/java/testPackage/Username_Class.java
@@ -0,0 +1,10 @@
+package testPackage;
+
+public class Username_Class {
+
+	public static void main(String[] args) {
+System.out.println("This is my Username class");
+
+	}
+
+}
